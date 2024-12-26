046d04702b478eb26a9264ab51c2de82068b909c
Anas Ahmed <anasahmed700@gmail.com>
2024-12-26 19:17:07 +0500

    creating model and adding annotations

---
+++ DairyApp/Models/DiaryEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DairyApp.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+        [Required] // Annotating for required value
+        public string Title { get; set; } = string.Empty; // Default value
+        public string? Content { get; set; } // Declare a nullable
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
