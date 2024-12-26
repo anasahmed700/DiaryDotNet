aaa4edf030149776927cb42d5d87f950253fdba2
Anas Ahmed <anasahmed700@gmail.com>
2024-12-26 19:37:41 +0500

    Create DiaryEntry.cs

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+        [Required] // Annotating for required value
+        public string Title { get; set; } = string.Empty; // Default value
+        public string? Content { get; set; } // Declare a nullable
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
