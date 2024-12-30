9507155523cab726d2f082215bf012fc2ce771c0
Anas Ahmed <anasahmed700@gmail.com>
2024-12-30 11:52:13 +0500

    added seeder for diary entries

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using DiaryApp.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace DiaryApp.Data
 {
@@ -7,14 +8,48 @@ namespace DiaryApp.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
-        public DbSet<DiaryEntry> DiaryEntries { get; set; }
         // Four steps to add a table
         // 1. Create a model class
         // 2. Add DB Set
         // 3. add-migration AddDiaryEntryTable
         // 4. update-database
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry
+                {
+                    Id = 1,
+                    Title = "Went Hiking",
+                    Content = "Went hiking with Ali",
+                    CreatedAt = DateTime.Now
+                },
+                new DiaryEntry
+                {
+                    Id = 2,
+                    Title = "Went Shopping",
+                    Content = "Went shopping with Ali",
+                    CreatedAt = DateTime.Now
+                },
+                new DiaryEntry
+                {
+                    Id = 3,
+                    Title = "Went Diving",
+                    Content = "Went diving with Ali",
+                    CreatedAt = DateTime.Now
+                }
+            );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        }
     }
 }
