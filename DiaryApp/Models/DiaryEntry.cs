3abd19dbeb9cd072f0fefbbe5a3f419601997b60
Anas Ahmed <anasahmed700@gmail.com>
2024-12-31 20:36:11 +0500

    added client side validations

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -5,9 +5,11 @@ namespace DiaryApp.Models
     public class DiaryEntry
     {
         public int Id { get; set; }
-        [Required] // Annotating for required value
+        [Required(ErrorMessage = "Please enter a title!")] // Annotating for required value
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Title must be between 3 to 100 characters!")]
         public string Title { get; set; } = string.Empty; // Default value
         public string? Content { get; set; } // Declare a nullable
+        [Required(ErrorMessage = "Please select a date!")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
