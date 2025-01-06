5e7d361a56790d9e091bd5a844602ea2f9716343
Anas Ahmed <anasahmed700@gmail.com>
2025-01-06 18:55:07 +0500

    adding server side validation

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -25,9 +25,16 @@ namespace DiaryApp.Controllers
         [HttpPost]
         public IActionResult Create(DiaryEntry obj)
         {
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title is too short!");
+            }
+
+            if (!ModelState.IsValid) return View(obj);
             _db.DiaryEntries.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
+
         }
     }
 }
--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -6,7 +6,7 @@ namespace DiaryApp.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Please enter a title!")] // Annotating for required value
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Title must be between 3 to 100 characters!")]
+        //[StringLength(50, MinimumLength = 3, ErrorMessage = "Title must be between 3 to 100 characters!")]
         public string Title { get; set; } = string.Empty; // Default value
         public string? Content { get; set; } // Declare a nullable
         [Required(ErrorMessage = "Please select a date!")]
